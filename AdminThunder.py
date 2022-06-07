38378e7933f3d73250ec86cd6be81ad3bf596255
ejlam <furius22@gmail.com>
2022-06-07 14:26:14 -0400

    block users from muting mods with :pleasestop:

--- AdminThunder.py
+++ AdminThunder.py
@@ -7,12 +7,16 @@ try:
     TOKEN = os.environ['TOKEN']
     MOD_CHANNEL = os.environ['MOD_CHANNEL']
     USER_COUNT = int(os.environ['USER_COUNT'])
+    IGNORE_CHANNELS = list(os.environ['IGNORE_CHANNELS'])
+    MODS = list(os.environ['MODS'])
 except:
     config = configparser.ConfigParser()
     config.read('config.cfg')
     TOKEN = config['INDEV']['TOKEN']
     MOD_CHANNEL = config['INDEV']['MOD_CHANNEL']
     USER_COUNT = 1
+    IGNORE_CHANNELS = [715969933980467263]
+    MODS = None
 
 # Create a new interactions Discord client
 intents = discord.Intents.default()
@@ -30,9 +34,18 @@ async def on_ready():
 @client.event
 async def on_raw_reaction_add(payload):
     #check if the reaction is the one we want
+
+
     if payload.emoji.name.lower() == 'pleasestop':
         channel = client.get_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
+
+        if payload.channel_id in IGNORE_CHANNELS:
+            return
+
+        if message.author.id in MODS:
+            return
+        
         reaction = discord.utils.get(message.reactions, emoji=payload.emoji)
         users = set()
         async for member in reaction.users():
@@ -47,10 +60,10 @@ async def on_raw_reaction_add(payload):
             files = None
 
         if reaction.count >= USER_COUNT:
-            log_channel = client.get_channel(MOD_CHANNEL)
+            log_channel = client.get_channel(int(MOD_CHANNEL))
             user = client.get_user(message.author.id)
 
-            await log_channel.send(f"{user.display_name} sent the following message in {f'<#{channel.id}>'}, which was deemed inappropriate by at least three users: {', '.join(member.name for member in users)}\n\n {message.content}", files=files)
+            await log_channel.send(f"Offending User: {user.display_name} \nChannel: {f'<#{channel.id}>'} \nOffended Users: {', '.join(member.name for member in users)}\nMessage:\n\n {message.content}", files=files)
             
             await channel.send(f'{user.mention}, your post has been deleted and is under further review.')
 
@@ -58,6 +71,7 @@ async def on_raw_reaction_add(payload):
 
 @client.event
 async def on_message(message):
+
     if message.author.id == client.user.id:
         #delete all files in /tmp/
         for file in os.listdir(r'./tmp/'):
